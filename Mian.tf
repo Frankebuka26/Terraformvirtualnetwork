8e64514993a09e191dbc23f1ca7cfd4619904d47
Franklyn Oyinze <FranklynOyinze@gmail.com>
2025-04-07 03:48:12 +0100

    commit to main

--- Mian.tf
+++ Mian.tf
@@ -11,17 +11,19 @@ provider "azurerm" {
   features {}
 }
 
+# resource name points to tag for resource name.
 resource "azurerm_resource_group" "Rg1" {
   name     = var.resource_group_name
   location = "West US"
   tags     = var.tags
+  # Var.tags was used as format to point to varibale for tags.
 }
 
 resource "azurerm_virtual_network" "Vnet1" {
   name                = "Vnet1"
-  location            = "East US"
+  location            = "East US" # Virtual network location is different from resource location.
   resource_group_name = azurerm_resource_group.Rg1.name
-  address_space       = ["10.123.0.0/16"]
+  address_space       = ["10.123.0.0/16"] # Subnet mask range defined.
 
   tags = var.tags
 }
--- Variables.tf
+++ Variables.tf
@@ -1,8 +1,10 @@
+# This variable for resource group name
 variable "resource_group_name" {
   type    = string
   default = "Rg1"
 }
 
+# This variable for tags.
 variable "tags" {
   type        = map(string)
   description = "Common tags"
